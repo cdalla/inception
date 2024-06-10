0834416cbbe5188dc55ed83803f29158b44dccba
cdalla <cdalla-s@student.codam.nl>
2024-06-10 12:15:42 -0400

    some mod

--- srcs/requirements/mariadb/tools/start-database.sh
+++ srcs/requirements/mariadb/tools/start-database.sh
@@ -4,11 +4,11 @@
 if ! [ -d "var/lib/mysql/$DATABASE_NAME" ]; then
 
 service start mariadb
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSW'; FLUSH PRIVILEGES;"
-mariadb -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
-mariadb -e "GRANT ALL ON $DATABASE_NAME.* TO '$USERDB_NAME'@'localhost' IDENTIFIED BY '$USERDB_PASSW';"
-mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSW';"
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -u root -p $MARIADB_ROOT_PASSW -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSW'; FLUSH PRIVILEGES;"
+mariadb -u root -p $MARIADB_ROOT_PASSW -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
+mariadb -u root -p $MARIADB_ROOT_PASSW -e "GRANT ALL ON $DATABASE_NAME.* TO '$USERDB_NAME'@'localhost' IDENTIFIED BY '$USERDB_PASSW';"
+mariadb -u root -p $MARIADB_ROOT_PASSW -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSW';"
+mariadb -u root -p $MARIADB_ROOT_PASSW -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p $MARIADB_ROOT_PASSW shutdown
 
 fi
--- srcs/requirements/wordpress/tools/wpconf.sh
+++ srcs/requirements/wordpress/tools/wpconf.sh
@@ -1,28 +1,26 @@
-if ! [[ -f "/var/www/wp-config.php" ]]; then
+if [ ! -f "/var/www/wp-config.php" ]; then
 
-echo "Wordepress configuration..."
+    echo "Wordepress configuration..."
 
-wp core download    --allow-root
+    wp core download    --allow-root
 
-wp config create	--allow-root \
-					--dbname=$DATABASE_NAME \
-					--dbuser=$USERDB_NAME \
-					--dbpass=$USERDB_PASSW \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+    wp config create	--allow-root \
+					    --dbname=$DATABASE_NAME \
+					    --dbuser=$USERDB_NAME \
+					    --dbpass=$USERDB_PASSW \
+					    --dbhost=mariadb:3306 --skip-check --path='/var/www/wordpress'
 
-sleep 5
+    wp core install     --allow-root \
+                        --skip-email \
+                        --url="$DOMAIN_NAME" \
+                        --title="Inception" \
+                        --admin_name ="$ADMIN_WP" \
+                        --admin_password="$ADMIN_WP_PASSW"
 
-wp core install     --allow-root \
-                    --skip-email \
-                    --url="$DOMAIN_NAME" \
-                    --title="Inception" \
-                    --admin_name ="$ADMIN_WP" \
-                    --admin_password="$ADMIN_WP_PASSW"
-
-wp user create      --allow-root \
-                    "$USER_WP" \
-                    --skip-email \
-                    --user_pass="$USER_WP_PASSW"
+    wp user create      --allow-root \
+                        "$USER_WP" \
+                        --skip-email \
+                        --user_pass="$USER_WP_PASSW"
 
 fi
 
