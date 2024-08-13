549bb0d2d7f18d8e8ba2714682e06eb72397483e
cdalla-s <cdalla-s@codam.42.nl>
2024-08-13 13:46:08 +0200

    still no wordpress visible

--- srcs/requirements/wordpress/tools/wpconf.sh
+++ srcs/requirements/wordpress/tools/wpconf.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-if [ ! -f ./wp-config.php ]; then
+if [ ! -f /var/www/html/wp-config.php ]; then
+
+    sleep 10
 
     echo "Wordpress configuration..."
 
     wp core download    --allow-root
 
-    wp config create	--allow-root \
-					    --dbname=$DATABASE_NAME \
-					    --dbuser=$USERDB_NAME \
-					    --dbpass=$USERDB_PASSW \
-					    --dbhost=mariadb:3306
+    wp config create	--allow-root --dbname=$DATABASE_NAME --dbuser=$USERDB_NAME --dbpass=$USERDB_PASSW --dbhost=mariadb:3306
 
-    wp core install     --allow-root \
-                        --skip-email \
-                        --url=$DOMAIN_NAME \
-                        --title="Inception" \
-                        --admin_name =$ADMIN_WP \
-                        --admin_password=$ADMIN_WP_PASSW
+    wp core install     --allow-root --admin_email=$ADMIN_MAIL --url=$DOMAIN_NAME --title="Inception" --admin_name=$ADMIN_WP --admin_password=$ADMIN_WP_PASSW
 
-    wp user create      --allow-root \
-                        $USER_WP \
-                        --skip-email \
-                        --user_pass="$USER_WP_PASSW"
+    wp user create      --allow-root $USER_WP $USER_MAIL --user_pass="$USER_WP_PASSW"
 
 fi
 
